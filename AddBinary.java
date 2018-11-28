06ca77b1d7dcde444dee69c987b485ae371f6c24
morishima <ryoryo311@gmail.com>
2018-11-29 00:19:19 +0900

    Enabled to give String binary from command line

--- AddBinary.java
+++ AddBinary.java
@@ -4,7 +4,7 @@ class Solution{
         int i = a.length() - 1;
         int j = b.length() - 1;
         int carry = 0;
-        while(i >= 0 || j >= 0){
+        while (i >= 0 || j >= 0){
             int sum = carry;
             if (i >= 0){
                 sum += a.charAt(i--) - '0';
@@ -15,15 +15,29 @@ class Solution{
             sb.insert(0, sum % 2);
             carry = sum / 2;
        }
-       if(carry > 0){
+       if (carry > 0){
            sb.insert(0, 1);
        }
        return sb.toString();
     }
 
     public static void main(String[] args){
-        String a = "1011";
-        String b = "110";
-        System.out.println(addBinary(a, b));
+        if(args.length != 2){
+                System.out.println("Proper Usage: <java> <program> <binary1> <binary2>");
+                System.exit(1);
+        }
+        for (int i=0; i<args[0].length(); i++){
+            if (args[0].charAt(i) != '0' && args[0].charAt(i) != '1'){
+                System.out.println("Invalid arguments");
+                System.exit(1);
+            }
+        }
+        for (int i=0; i<args[1].length(); i++){
+            if (args[1].charAt(i) != '0' && args[1].charAt(i) != '1'){
+                System.out.println("Invalid arguments");
+                System.exit(1);
+            }
+        }
+        System.out.println(addBinary(args[0], args[1]));
     }
 }
